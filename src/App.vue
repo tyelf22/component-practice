dbf1bd472008376a3295a71735430d1b301fc7f5
tyelf22 <tyelf22@hotmail.com>
2019-10-02 17:25:06 -0600

    added custom component

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,6 @@ export default {
   components: {
     PersonGrid,
     AustinWright
-
   },
   data: () => {
     return {
---
+++ src/developers/TysonElfors.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-card>
+        <v-card-title>{{ name }}</v-card-title>
+        <v-card-text>My custom component</v-card-text>
+    </v-card>
+</template>
+
+<script>
+export default {
+   data: () => {
+       return {
+           name: 'Tyson Elfors'
+       }
+   } 
+}
+</script>
+
+<style>
+
+</style>
