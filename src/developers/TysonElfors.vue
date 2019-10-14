44ff2dd40a7ba51013da5bdfee7f9da070fb8e20
tyson <tyelf22@hotmail.com>
2019-10-14 09:09:46 -0600

    added input field

--- src/developers/TysonElfors.vue
+++ src/developers/TysonElfors.vue
@@ -2,6 +2,7 @@
     <v-card>
         <v-card-title>{{ name }}</v-card-title>
         <v-card-text>My custom component</v-card-text>
+        <v-text-field counter="25" maxlength="25" label="input"></v-text-field>
     </v-card>
 </template>
 
@@ -9,7 +10,8 @@
 export default {
    data: () => {
        return {
-           name: 'Tyson Elfors'
+           name: 'Tyson Elfors',
+          
        }
    } 
 }
